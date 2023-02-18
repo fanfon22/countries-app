ae781712c1cab0e843f24c03239987f66aaf6b9f
LAPTOP-81EMASE2\tomik <tomas.juhasz@kosickaakademia.sk>
2023-02-18 15:26:52 +0100

    add layouts for add user and add button

--- app/src/main/java/com/juhasz/country/data/UserDao.kt
+++ app/src/main/java/com/juhasz/country/data/UserDao.kt
@@ -1,8 +1,23 @@
 package com.juhasz.country.data
 
 import androidx.room.Dao
-
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
-class UserDao {
+interface UserDao {
+
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+
 }
