ebc6086dad6d61e144019440774b2e495e4ef6f0
LAPTOP-81EMASE2\tomik <tomas.juhasz@kosickaakademia.sk>
2023-02-18 15:33:11 +0100

    editorActivity 20%

--- app/src/main/java/com/juhasz/country/EditorActivity.kt
+++ app/src/main/java/com/juhasz/country/EditorActivity.kt
@@ -2,10 +2,19 @@ package com.juhasz.country
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class EditorActivity : AppCompatActivity() {
+    private lateinit var name: EditText
+    private lateinit var username: EditText
+    private lateinit var email: EditText
+    private lateinit var btnSave: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+        name = findViewById(R.id.name)
+        username = findViewById(R.id.username)
+        email = findViewById(R.id.email)
+        btnSave = findViewById(R.id.btn_save)
     }
 }
